ba8bb5a5974e1e9b5e93420ed69c4a2519785a14
Aditya raj <raj49274@gmail.com>
2018-03-25 21:10:28 +0530

    updated my project-news file

--- project_news.py
+++ project_news.py
@@ -4,34 +4,42 @@ db = psycopg2.connect("dbname=news")
 c = db.cursor()
 
 # first question
-query1 = "SELECT path, COUNT(*) as num FROM log\
+query1 = "SELECT articles.slug , put.path, put.num, articles.title from articles,\
+         (SELECT path, COUNT(*) as num FROM log\
          GROUP BY path ORDER BY num desc\
-         LIMIT 10 OFFSET 1"
+         LIMIT 3 OFFSET 1) as put order by num desc"
 c.execute(query1)
 posts = c.fetchall()
 print "What are the most popular three articles of all time?"
 print
-print "Candidate is jerk, alleges rival--- ", posts[0][1], "views"
-print "Bears love berries, alleges bear--- ", posts[1][1], "views"
-print "Bad things gone, say good people--- ", posts[2][1], "views"
+for post in posts:
+    if post[0] == post[1][9:]:
+        print post[3],"--", post[2], "views"
 
 # second question
-query2 = "SELECT name FROM authors"
+
+query2 = "SELECT authors.name, sum(tiger.num) as views from authors,\
+          (SELECT Substring(path, 10) as paths, COUNT(*) as num FROM log\
+          GROUP BY path ORDER BY num desc\
+          LIMIT 10 OFFSET 1) as tiger\
+          join\
+          (SELECT author, slug from articles) as lion \
+          on tiger.paths = lion.slug \
+          where authors.id = lion.author \
+          group by authors.name order by views desc"
 c.execute(query2)
 oath = c.fetchall()
 print
 print "Who are the most popular article authors of all time?"
 print
-print oath[0][0], "--", posts[1][1] + posts[3][1] + posts[6][1] + posts[7][1],\
-      "views"
 
-print oath[1][0], "--", posts[0][1] + posts[4][1], "views"
-print oath[2][0], "--", posts[2][1], "views"
-print oath[3][0], "--", posts[5][1], "views"
+for name in oath:
+    print name[0], "--", name[1], "views"
+
 
 # third question
 
-query3 = "SELECT max((cast(tiger.num2 as INTEGER)/cast(lion.num as FLOAT))*100)\
+query3 = "SELECT lion.days, (cast(tiger.num2 as INTEGER)/cast(lion.num as FLOAT))*100\
          as error from \
          (SELECT date(time) as days, count(status) as num from log\
          group by days) as lion\
@@ -46,7 +54,12 @@ c.execute(query3)
 rush = c.fetchall()
 print
 print "On which days did more than 1% of requests lead to errors?"
-print "july 17 2016 --", round(rush[0][0], 1), "%", "error"
+print
+for item in rush:
+    if item[1] > 2:
+        print item[0], "--", round(item[1], 2), "%" + " error"
+
 
 
 db.close()
+
