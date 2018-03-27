908a509cb6509115e2bac13ead3bfcec8f76571c
Aditya raj <raj49274@gmail.com>
2018-03-27 12:07:09 +0530

    adding function in projec_news.py

--- project_news.py
+++ project_news.py
@@ -1,65 +1,88 @@
+#!/usr/bin/env python
 import psycopg2
+import sys
 
-db = psycopg2.connect("dbname=news")
+
+def connect(database_name):
+    """Connect to the database.  Returns a database connection."""
+    try:
+        db = psycopg2.connect(dbname=database_name)
+        return db
+
+    except psycopg2.Error as e:
+        # THEN you could print an error
+        # and perhaps exit the program
+        print ("Unable to connect to database")
+        sys.exit(1)
+
+db = connect("news")
 c = db.cursor()
 
+
+def execute_query(query):
+    c.execute(query)
+    posts = c.fetchall()
+    return posts
+
+
 # first question
-query1 = "SELECT articles.slug , put.path, put.num, articles.title from articles,\
-         (SELECT path, COUNT(*) as num FROM log\
-         GROUP BY path ORDER BY num desc\
-         LIMIT 3 OFFSET 1) as put order by num desc"
-c.execute(query1)
-posts = c.fetchall()
-print "What are the most popular three articles of all time?"
-print
-for post in posts:
-    if post[0] == post[1][9:]:
-        print post[3],"--", post[2], "views"
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
+    query = """
+        SELECT title, count(*) AS views
+        FROM log
+        JOIN articles
+            ON log.path = concat('/article/', articles.slug)
+        GROUP BY title
+        ORDER BY views desc
+        LIMIT 3;"""
+    results = execute_query(query)
+    # add code to print results
+    print "What are the most popular three articles of all time?"
+    for result in results:
+        print result[0], "--", result[1], "views"
+
 
 # second question
-
-query2 = "SELECT authors.name, sum(tiger.num) as views from authors,\
-          (SELECT Substring(path, 10) as paths, COUNT(*) as num FROM log\
-          GROUP BY path ORDER BY num desc\
-          LIMIT 10 OFFSET 1) as tiger\
-          join\
-          (SELECT author, slug from articles) as lion \
-          on tiger.paths = lion.slug \
-          where authors.id = lion.author \
-          group by authors.name order by views desc"
-c.execute(query2)
-oath = c.fetchall()
-print
-print "Who are the most popular article authors of all time?"
-print
-
-for name in oath:
-    print name[0], "--", name[1], "views"
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+    query = """SELECT authors.name, sum(tiger.num) as views from authors,
+              (SELECT Substring(path, 10) as paths, COUNT(*) as num FROM log
+              GROUP BY path ORDER BY num desc
+              LIMIT 10 OFFSET 1) as tiger
+              join
+              (SELECT author, slug from articles) as lion
+              on tiger.paths = lion.slug
+              where authors.id = lion.author
+              group by authors.name order by views desc;"""
+    results = execute_query(query)
+    # add code to print results
+    print
+    print "Who are the most popular article authors of all time?"
+    for result in results:
+        print result[0], "--", result[1], "views"
 
 
 # third question
+def print_errors_over_one():
+    query = """SELECT lion.days, (cast(tiger.num2 as INTEGER)/cast(lion.num as FLOAT))*100
+             as error from
+             (SELECT date(time) as days, count(status) as num from log
+             group by days) as lion
+             join
+             (SELECT date(time) as days, count(*) as num2 from log
+             where status = '404 NOT FOUND'
+             GROUP BY days) as tiger
+             on lion.days = tiger.days"""
+    results = execute_query(query)
+    print
+    print "On which days did more than 1% of requests lead to errors?"
+    for result in results:
+        if result[1] > 2:
+            print result[0], "--", round(result[1], 2), "%" + " error"
 
-query3 = "SELECT lion.days, (cast(tiger.num2 as INTEGER)/cast(lion.num as FLOAT))*100\
-         as error from \
-         (SELECT date(time) as days, count(status) as num from log\
-         group by days) as lion\
-         join\
-         (SELECT date(time) as days, count(*) as num2 from log \
-         where status = '404 NOT FOUND'\
-         GROUP BY days) as tiger\
-         on lion.days = tiger.days"
-
-
-c.execute(query3)
-rush = c.fetchall()
-print
-print "On which days did more than 1% of requests lead to errors?"
-print
-for item in rush:
-    if item[1] > 2:
-        print item[0], "--", round(item[1], 2), "%" + " error"
-
-
-
-db.close()
-
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
+    db.close()
